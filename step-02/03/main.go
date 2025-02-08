3437a8c6b3d5aca255a647ecd75ea6bf057d26ab
YovaDiaz <thebigyovadiaz@gmail.com>
2025-02-08 00:47:44 -0300

    Challenge: the conference lunch

---
+++ step-02/03/main.go
@@ -0,0 +1,65 @@
+// Challenge: The Conference Lunch
+// Task: given a defined list of resources implement a function
+//		that simulates the concurrent allocation of resources to consumer goroutines.
+
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+// the number of attendees we need to serve lunch
+const consumerCount = 300
+
+// foodCourses represents the types of resources to pass to the consumers
+var foodCourse = []string{
+	"Caprese Salad",
+	"Spaghetti Carbonara",
+	"Vanilla Panna Cotta",
+}
+
+// takeLunch is the consumer function for the lunch simulation
+// Change the signature of this function as required
+func takeLunch(name string, in []chan string, done chan<- struct{}) {
+	for _, ch := range in {
+		log.Printf("%s eats %s.\n", name, <-ch)
+	}
+	done <- struct{}{}
+}
+
+// serveLunch is the producer function for the lunch simulation.
+// Change the signature of this function as required
+func serveLunch(course string, out chan<- string, done <-chan struct{}) {
+	for {
+		select {
+		case out <- course:
+		case <-done:
+			return
+		}
+	}
+}
+
+func main() {
+	log.Printf("Welcome to the conference luch! Serving %d attendees.\n", consumerCount)
+
+	var courses []chan string
+	doneEating := make(chan struct{})
+	doneServing := make(chan struct{})
+
+	for _, c := range foodCourse {
+		ch := make(chan string)
+		courses = append(courses, ch)
+		go serveLunch(c, ch, doneServing)
+	}
+
+	for i := 0; i < consumerCount; i++ {
+		name := fmt.Sprintf("Attendee %d", i)
+		go takeLunch(name, courses, doneEating)
+	}
+
+	for i := 0; i < consumerCount; i++ {
+		<-doneEating
+	}
+	close(doneServing)
+}
